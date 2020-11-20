481f33347718798149e9b028963fb2b9bd0ab9f4
Aakash <aakashk.kvjp58@gmail.com>
2020-11-20 22:25:41 +0530

    .

--- Core/Inc/esp_at.h
+++ Core/Inc/esp_at.h
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdint.h>
 #include <string.h>
-   
 
 class ESP_AT{
     UART_HandleTypeDef* _uart;
@@ -9,7 +8,12 @@ class ESP_AT{
     char *_pass;
     char* _apiKey;
     char _host[23]; // \"api.thingspeak.com\"\0
+    static const uint8_t _bufferSize = 50;
+	uint8_t _buffer[_bufferSize]; // to store serial data coming from esp8266
     void _setSingleConn();
+    const size_t _timeout = 50; // 2 sec
+    const size_t _timeoutN = 100;
+    const size_t _maxRetries = 4;
 
     public:
     ESP_AT(UART_HandleTypeDef* uart);
