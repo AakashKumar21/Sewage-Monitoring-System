843f053f4582b52ee4a331d1ddcbee91d02af1bf
ADITYA-KUMAR-MISHRA <mishraaditya805@gmail.com>
2020-11-22 20:49:03 +0530

    Added code of distance sensor

---
+++ Core/Inc/distance.h
@@ -0,0 +1,21 @@
+/************************************************************************************************
+*
+*
+*
+*
+*
+*                     @Author : ADITYA-KUMAR-MISHRA
+*		      @Date   : 22:11:2020
+*
+*
+*
+*
+*
+**************************************************************************************************/
+#ifndef _distance_h_
+#define _distance_h_
+//Function to return the distance of the object 
+//No input 
+//returns float
+float distance() ;
+#endif /*distance_h_ */
---
+++ Core/Src/distance.c
@@ -0,0 +1,41 @@
+/***********************************************************************************************
+ * 
+ * 
+ * 
+ * 
+ * @Author  : ADITYA-KUMAR-MISHRA
+ * @DATE    :  22/11/2020  
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * ************************************************************************************************/
+#include "distance.h"
+//function to calculate and return the distance of the object
+// No input
+// Returns distance as float in cm
+float distance(void){
+    uint32_t local_time;
+    float distance;
+    local_time=0;
+    HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET);  // pull the TRIG pin HIGH
+    delay(2);  // wait for 2 us
+
+
+    HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_SET);  // pull the TRIG pin HIGH
+    delay(10);  // wait for 10 us
+    HAL_GPIO_WritePin(TRIG_PORT, TRIN_PIN, GPIO_PIN_RESET);  // pull the TRIG pin low
+
+    // read the time for which the pin is high
+
+    while (!(HAL_GPIO_ReadPin(ECHO_PORT, ECHO_PIN)));  // wait for the ECHO pin to go high
+    while (HAL_GPIO_ReadPin(ECHO_PORT, ECHO_PIN))    // while the pin is high
+    {
+        local_time++;   // measure time for which the pin is high
+        delay (1);
+    }
+    return (float)(local_time * 0.343/2);
+}
